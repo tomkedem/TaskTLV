8141b9bb2811636c1c60e3384cf8669dbc6cc114
Tomer Kedem <tompha1@hotmail.com>
2024-11-13 20:48:07 +0200

    Adding a design for a better look

--- Client/product-management/src/app/components/login/login.component.ts
+++ Client/product-management/src/app/components/login/login.component.ts
@@ -1,29 +1,40 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
-import { FormsModule } from '@angular/forms';
-
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
-  username = '';
-  password = '';
+  loginForm: FormGroup;
   errorMessage = '';
 
   constructor(
+    private fb: FormBuilder,
     private authService: AuthService,
     private router: Router
-  ) {}
+  ) {
+    // Initialize the form with validators
+    this.loginForm = this.fb.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+  }
 
   login(): void {
-    this.authService.login(this.username, this.password).subscribe(
+    if (this.loginForm.invalid) {
+      return;
+    }
+
+    const { username, password } = this.loginForm.value;
+
+    this.authService.login(username, password).subscribe(
       (response) => {
         // Save JWT and role to local storage
         localStorage.setItem('token', response.token);
